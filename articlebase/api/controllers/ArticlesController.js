fcc24282550cfb0716504a15836aa60cc158849a
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-10-18 16:22:48 +0100

    set up controller

--- articlebase/api/controllers/ArticlesController.js
+++ articlebase/api/controllers/ArticlesController.js
@@ -6,7 +6,30 @@
  */
 
 module.exports = {
-  
-
+    loadArticleList:(req,res)=>{
+        Articles.find({}).exec(function(err, articles){
+            if(err){
+                res.send(500, {error:'Database Error'});
+            } else{
+                return res.view('list',{articles});
+            }
+        })
+    },
+    addArticle:(req,res)=>{
+        return res.view('add');
+    },
+    create:(req,res)=>{
+        const { title,body } = req.body;
+        
+        Articles.create({
+            title,
+            body
+        }).exec((err)=>{
+            if (err){
+                res.send(500, {error:'Database Error'});
+            }
+            res.redirect('/articles/list');
+        });
+    }
 };
 
